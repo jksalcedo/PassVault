7ec03d1763a7864ac061b4a8d09e5c4d939b174e
jksalcedo <jks.create@gmail.com>
2025-11-05 09:05:04 +0000

    Bump version from 0.2-alpha to 0.3-alpha
    The `versionCode` is incremented from 2 to 3, and the `versionName` is updated from "0.2-alpha" to "0.3-alpha".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jksalcedo.passvault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2-alpha"
+        versionCode = 3
+        versionName = "0.3-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
