397811f78ec9b5c50518ff68c95abda75ef1c2e1
jksalcedo <jks.create@gmail.com>
2026-02-11 06:06:07 +0000

    feat: improve autofill field detection and entry matching logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.jksalcedo.passvault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 33
-        versionName = "1.8.0"
+        versionCode = 34
+        versionName = "1.9.0-beta1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
