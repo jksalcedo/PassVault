eef36e7bd44d837dee5d5030a52e37452844eccd
jksalcedo <jks.create@gmail.com>
2026-06-15 04:53:13 +0000

    feat: bump version to 2.0.0 and update build tools

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 38
-        versionName = "1.9.3"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
